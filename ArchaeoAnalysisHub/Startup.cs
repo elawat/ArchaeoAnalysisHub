5778599ccf52626343c4c1eaa0bd4b1d7216494e
elawat <elawatroba@gmail.com>
2019-05-14 18:37:23 +0200

    Add project files.

---
+++ ArchaeoAnalysisHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArchaeoAnalysisHub.Startup))]
+namespace ArchaeoAnalysisHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
